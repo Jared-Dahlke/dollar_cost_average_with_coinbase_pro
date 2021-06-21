20566244021d67b669e40c4a66b6b1230640d948
Jared Dahlke <jared@sightly.com>
2021-06-21 10:57:45 -0700

    update

---
+++ app.js
@@ -0,0 +1,10 @@
+
+var express = require(‘express’);
+var port = process.env.PORT || 3000;
+var app = express();
+app.get(‘/’, function (req, res) {
+ res.send(JSON.stringify({ Hello: ‘World’}));
+});
+app.listen(port, function () {
+ console.log(`Example app listening on port !`);
+});
