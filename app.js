cbfe89a464df4f71b824eee3c7b7131c5f16d5d6
purplepark849 <jared@sightly.com>
2021-06-27 07:07:08 -0700

    if funds not available then deposit more funds

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
 var express = require('express')
-var port = process.env.PORT || 3000
+var port = process.env.PORT
 var app = express()
 const CoinbasePro = require('coinbase-pro') // https://www.npmjs.com/package/coinbase-pro
 require('dotenv').config()
 const secret = process.env.SECRET
 const key = process.env.KEY
 const passphrase = process.env.PASSPHRASE
+const apiURI = 'https://api.pro.coinbase.com'
+const authedClient = new CoinbasePro.AuthenticatedClient(
+	key,
+	secret,
+	passphrase,
+	apiURI
+)
 
-const dollarAmountToBuy = 5
+const buy_frequency_hours = 3
+const dollarAmountToBuy = 5 // min is 5
+const deposit_amount = 100
 
-async function buyBitcoin() {
-	const apiURI = 'https://api.pro.coinbase.com'
-	const authedClient = new CoinbasePro.AuthenticatedClient(
-		key,
-		secret,
-		passphrase,
-		apiURI
-	)
+async function startProcess() {
+	const accounts = await authedClient.getAccounts()
+	let usdAccountId = accounts.filter((account) => account.currency === 'USD')[0]
+		.id
+	const account = await authedClient.getAccount(usdAccountId)
+	if (Number(account.available) < Number(dollarAmountToBuy)) {
+		let success = await makeDeposit(authedClient)
+	}
+	buyBitcoin(authedClient)
 
+	setTimeout(() => {
+		startProcess()
+	}, buy_frequency_hours * 3600000)
+}
+
+async function buyBitcoin(authedClient) {
 	const params = {
 		side: 'buy',
 		type: 'market',
@@ -25,19 +41,32 @@ async function buyBitcoin() {
 		funds: dollarAmountToBuy
 	}
 
-	await authedClient.placeOrder(params, console.log('placed'))
+	try {
+		let order = await authedClient.placeOrder(params)
+		console.log(
+			`the bot bought ${order.specified_funds} worth of bitcoin at ${order.created_at}`
+		)
+	} catch (err) {
+		console.log('error buying bitcoin:')
+		console.log(err)
+	}
+}
 
-	console.log('you just bought bitcoin!')
-
-	// 14400000 <- 4 hours
-	// 21600000 <- 6 hours
-	// 43200000 <- 12 hours
-	setTimeout(() => {
-		buyBitcoin()
-	}, 21600000)
+async function makeDeposit(authedClient) {
+	const paymentMethods = await authedClient.getPaymentMethods()
+	let instantBuyPaymentMethodId = paymentMethods.filter(
+		(methods) => methods.instant_buy === true
+	)[0].id
+	let deposit = await authedClient.depositPayment({
+		amount: deposit_amount, // dollarAmountToBuy * 10,
+		currency: 'USD',
+		payment_method_id: instantBuyPaymentMethodId
+	})
+	console.log(`the bot deposited ${deposit.amount} USD at ${new Date()}`)
+	return true
 }
 
 app.listen(port, function () {
-	console.log(`Example app listening on port !`)
-	buyBitcoin()
+	console.log(`app listening on port ${7000}`)
+	startProcess()
 })
