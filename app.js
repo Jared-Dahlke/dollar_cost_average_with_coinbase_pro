7c4643c08d653a74d1be3d704415eb0213703ff2
purplepark849 <jared@sightly.com>
2021-07-28 08:49:32 -0700

    remove unused variables from app.js file

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 var express = require('express')
 var port = process.env.PORT
 var app = express()
-const CoinbasePro = require('coinbase-pro') // https://www.npmjs.com/package/coinbase-pro
 require('dotenv').config()
-const secret = process.env.SECRET
-const key = process.env.KEY
-const passphrase = process.env.PASSPHRASE
-const apiURI = 'https://api.pro.coinbase.com'
 
 const buy_frequency_hours = process.env.BUY_FREQUENCY_HOURS
 const dollar_amount_to_buy_per_purchase =
@@ -21,19 +16,12 @@ const {
 } = require('./services/coinbasepro')
 
 async function startDcaProcess() {
-	const authedClient = new CoinbasePro.AuthenticatedClient(
-		key,
-		secret,
-		passphrase,
-		apiURI
-	)
-
 	const accounts = await getAccounts()
 	let usdAccountId = accounts.filter((account) => account.currency === 'USD')[0]
 		.id
 	const account = await getAccount(usdAccountId)
 	if (Number(account.available) < Number(dollar_amount_to_buy_per_purchase)) {
-		makeDeposit(authedClient)
+		makeDeposit()
 		//note: the depositPayment method in the coinbase api works, but
 		//it shows an error, there is no way of knowing when it is finished
 		//depositing, hence the settimeout here:
@@ -41,7 +29,7 @@ async function startDcaProcess() {
 			startDcaProcess()
 		}, 30000)
 	} else {
-		buyBitcoin(authedClient)
+		buyBitcoin()
 		setTimeout(() => {
 			startDcaProcess()
 		}, buy_frequency_hours * 3600000)
@@ -51,5 +39,5 @@ async function startDcaProcess() {
 app.listen(port, function () {
 	console.log(`app listening on port ${7000}`)
 	startDcaProcess()
-	//	calculatePurchasesSince('2021-06-30')
+	//calculatePurchasesSince('2021-07-20')
 })
